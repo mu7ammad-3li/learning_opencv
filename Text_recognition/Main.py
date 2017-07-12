edf4e45ce3b2430c671473027775bfd75525c3aa
Muhammad Ali <mu7ammad.3lii@gmail.com>
2017-07-12 22:49:54 +0200

    text recognition edited

--- Text_recognition/Main.py
+++ Text_recognition/Main.py
@@ -27,53 +27,55 @@ def main():
         return                                                          # and exit program
     # end if
 
-    imgOriginalScene  = cv2.imread("c2.jpg")               # open image
+#    imgOriginalScene  = cv2.imread("c2.jpg")               # open image
+    cap_stream = cv2.VideoCapture(0)
+    while(cap_stream.isOpened()):
+        imgOriginalScene = cap_stream.read()
+        if imgOriginalScene is None:                            # if image was not read successfully
+            print ("\nerror: image not read from file \n\n")      # print error message to std out
+            os.system("pause")                                  # pause so user can see error message
+            return                                              # and exit program
+            # end if
 
-    if imgOriginalScene is None:                            # if image was not read successfully
-        print ("\nerror: image not read from file \n\n")      # print error message to std out
-        os.system("pause")                                  # pause so user can see error message
-        return                                              # and exit program
-    # end if
+            listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
 
-    listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)           # detect plates
+            listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
 
-    listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)        # detect chars in plates
+            cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
 
-    cv2.imshow("imgOriginalScene", imgOriginalScene)            # show scene image
+            if len(listOfPossiblePlates) == 0:                          # if no plates were found
+                print ("\nno text were detected\n")             # inform user no text were found
+            else:                                                       # else
+                    # if we get in here list of possible plates has at leat one plate
 
-    if len(listOfPossiblePlates) == 0:                          # if no plates were found
-        print ("\nno text were detected\n")             # inform user no text were found
-    else:                                                       # else
-                # if we get in here list of possible plates has at leat one plate
+                    # sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
+                    listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
 
-                # sort the list of possible plates in DESCENDING order (most number of chars to least number of chars)
-        listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
+                    # suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
+                    licPlate = listOfPossiblePlates[0]
 
-                # suppose the plate with the most recognized chars (the first plate in sorted by string length descending order) is the actual plate
-        licPlate = listOfPossiblePlates[0]
+                    cv2.imshow("imgPlate", licPlate.imgPlate)           # show crop of plate and threshold of plate
+                    cv2.imshow("imgThresh", licPlate.imgThresh)
 
-        cv2.imshow("imgPlate", licPlate.imgPlate)           # show crop of plate and threshold of plate
-        cv2.imshow("imgThresh", licPlate.imgThresh)
+                    if len(licPlate.strChars) == 0:                     # if no chars were found in the plate
+                        print ("\nno characters were detected\n\n" )      # show message
+                    return                                          # and exit program
+                    # end if
 
-        if len(licPlate.strChars) == 0:                     # if no chars were found in the plate
-            print ("\nno characters were detected\n\n" )      # show message
-            return                                          # and exit program
-        # end if
+                    drawRedRectangleAroundPlate(imgOriginalScene, licPlate)             # draw red rectangle around plate
 
-        drawRedRectangleAroundPlate(imgOriginalScene, licPlate)             # draw red rectangle around plate
+                    print( "\nText read from image = " + licPlate.strChars + "\n" )      # write license plate text to std out
+                    print ("----------------------------------------")
 
-        print( "\nText read from image = " + licPlate.strChars + "\n" )      # write license plate text to std out
-        print ("----------------------------------------")
+                    writeLicensePlateCharsOnImage(imgOriginalScene, licPlate)           # write license plate text on the image
 
-        writeLicensePlateCharsOnImage(imgOriginalScene, licPlate)           # write license plate text on the image
+                    cv2.imshow("imgOriginalScene", imgOriginalScene)                # re-show scene image
 
-        cv2.imshow("imgOriginalScene", imgOriginalScene)                # re-show scene image
+                    cv2.imwrite("imgOriginalScene.png", imgOriginalScene)           # write image out to file
 
-        cv2.imwrite("imgOriginalScene.png", imgOriginalScene)           # write image out to file
+                    # end if else
 
-    # end if else
-
-    cv2.waitKey(0)					# hold windows open until user presses a key
+                    cv2.waitKey(0)					# hold windows open until user presses a key
 
     return
 # end main
